05aa15c255b330e5e0ab114bdb7a1b38d9ee852b
Leone Varando <lvarando@gmail.com>
2023-11-07 04:09:09 -0600

    Added logic to display connected guilds and their respective ids

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,8 +4,13 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
+    const guilds = client.guilds.cache.map(guild => guild);
+
     client.user.setActivity('for SUS messages...', { type: ActivityType.Watching })
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    for (const guild of guilds) {
+      console.log(`Joined ${guild.name}: ${guild.id}`);
+    }
   }
 }
