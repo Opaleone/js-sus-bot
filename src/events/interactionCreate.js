e01bb93632dd5b1d4bc77feb46b4851fda67ca61
Leone Varando <lvarando@gmail.com>
2023-11-06 21:55:55 -0600

    Added file to handle logic for interactionCreate event when /pp selected

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,21 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found`);
+      return;
+    }
+
+    try {
+      await command.execute(interaction);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
