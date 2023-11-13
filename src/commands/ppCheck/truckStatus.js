b416de347a3d78a73eee5c08eedb4897d400ef36
Leone Varando <lvarando@gmail.com>
2023-11-13 08:43:28 -0600

    Changed switch back to if else; Cleaned up code a bit

--- src/commands/ppCheck/truckStatus.js
+++ src/commands/ppCheck/truckStatus.js
@@ -5,15 +5,12 @@ const ppCheck = (size, status, curUser) => {
   dailyPP[curUser] = 1;
   console.log(dailyPP);
   
-  switch(size) {
-    case (size > 8):
-      return `You are ${size} inches ${status}.\n:eggplant:`;
-      break;
-    case (size <= 3):
-      return `You are ${size} inches ${status}.\n:microscope:`;
-      break;
-    default:
-      return `You are ${size} inches ${status}.\n:fried_shrimp:`;
+  if (size > 8) {
+    return `You are ${size} inches ${status}.\n:eggplant:`;
+  } else if (size <= 3) {
+    return `You are ${size} inches ${status}.\n:microscope:`;
+  } else {
+    return `You are ${size} inches ${status}.\n:fried_shrimp:`;
   }
 }
 
@@ -21,23 +18,20 @@ module.exports = {
   data: new SlashCommandBuilder()
       .setName('pp')
       .setDescription('PP CHECK!'),
-    async execute(interaction) {
-      if (interaction.guildId === '690308107007557652') {
-        if (interaction.channelId !== '1171394157475008572') {
-          await interaction.reply({ content: "Command must be run in #pp-check channel!", ephemeral: true });
-          return;
-        }
-      }
-
-      const truckStatus = ['hard', 'soft'];
-      const size = Math.round(Math.random() * 15);
-      const status = truckStatus[Math.floor(Math.random() * truckStatus.length)];
-      const curUser = interaction.user.username;
-
-      if (curUser in dailyPP) {
-        await interaction.reply({ content: "You've already checked your pp today. Try again tomorrow!", ephemeral: true });
-      } else {
-        await interaction.reply(ppCheck(size, status, curUser));
+  async execute(interaction) {
+    if (interaction.guildId === '690308107007557652') {
+      if (interaction.channelId !== '1171394157475008572') {
+        return await interaction.reply({ content: "Try this in #pp-check channel!", ephemeral: true });
       }
     }
+
+    const truckStatus = ['hard', 'soft'];
+    const size = Math.round(Math.random() * 15);
+    const status = truckStatus[Math.floor(Math.random() * truckStatus.length)];
+    const curUser = interaction.user.username;
+
+    if (curUser in dailyPP) return await interaction.reply({ content: "You've already checked your pp today. Try again tomorrow!", ephemeral: true });
+    
+    return await interaction.reply(ppCheck(size, status, curUser));
+  }
 }
