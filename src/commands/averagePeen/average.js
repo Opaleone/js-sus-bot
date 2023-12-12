c8c6cdecab192fd7fdd4cc3eb9e5969a3dd88d85
Leone Varando <lvarando@gmail.com>
2023-12-11 23:26:00 -0600

    Moved average calc func to average.js for average calc on command

--- src/commands/averagePeen/average.js
+++ src/commands/averagePeen/average.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const ppAvg = require('../../../utils/calculateAvg');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -14,6 +13,34 @@ module.exports = {
       }
 
       const curUser = interaction.user.username;
+      let ppAvg = {};
+
+      const file = fs.readFileSync('ppCheck.log', function(err, data) {
+        console.log(data);
+      })
+      
+      const fileArr = file.toString().split(`\n`);
+      fileArr.pop();
+      
+      for (let i = 0; i < fileArr.length; i++) {
+        const cur = fileArr[i];
+        const curArr = cur.split(' ');
+      
+        if (curArr[1] === curUser) {
+          let curObj = {
+            total: parseInt(curArr[3]),
+            count: 1
+          }
+        
+          if (ppAvg[curArr[1]]) {
+            ppAvg[curArr[1]].count++;
+            ppAvg[curArr[1]].total += parseInt(curArr[3]);
+          } else {
+            ppAvg[curArr[1]] = curObj;
+          }
+        }
+      }
+
       const average = (ppAvg[curUser].total / ppAvg[curUser].count).toFixed(2);
 
       if (ppAvg[curUser]) return await interaction.reply(`Your average peen size is ${average} inches.`); 
