8050cbe72b0df556878325184a9bb55c94b08584
Leone Varando <lvarando@gmail.com>
2024-02-14 01:35:58 -0600

    Added new command
    Added new command to let users add new words and responses to db for bot to look for and use as responses

---
+++ src/commands/add/add.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const config = require('../../../config.json');
+const restrictionFinder = require('../../../utils/restrictChannels');
+const { default: axios } = require('axios');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+      .setName('add')
+      .setDescription('Add a suspicious word or phrase to respond with')
+      .addStringOption(option => {
+        return option
+          .setName('susword')
+          .setDescription('Suspicious word to add')
+      })
+      .addStringOption(option => {
+        return option
+          .setName('susresponse')
+          .setDescription('Response for bot to use when sus word detected')
+      }),
+  async execute(interaction) {
+    try {
+      const tempObj = {
+        word: interaction.options.getString('susword') ?? null,
+        phrase: interaction.options.getString('susresponse') ?? null
+      }
+  
+      if (tempObj.word) {
+        await axios.post(`${config.baseUrl}/suspicious/${tempObj.word}`)
+      }
+  
+      if (tempObj.phrase) {
+        await axios.post(`${config.baseUrl}/responses/${tempObj.phrase}`)
+      }
+
+      await interaction.reply({ content: "Successfully added!", ephemeral: true });
+    } catch (e) {
+      const todayDate = new Date().toJSON();
+      const msg = `${todayDate}: ${e.message} ::add.js::\n`;
+
+      // Log error to file
+      fs.appendFile('errors.log', msg, (err) => {
+        if (err) console.error(err)
+      })
+    }
+  }
+}
